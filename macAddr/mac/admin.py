4a4b8088e69669734f387ee6a97d2fac55d4301d
Mals0812 <malavikasri644@gmail.com>
2024-07-14 23:59:09 -0700

    Delete macAddr directory

--- macAddr/mac/admin.py
+++
@@ -1,5 +0,0 @@
-"""This is the admin of the project"""
-from django.contrib import admin
-from .models import HubStatus
-# Register your models here.
-admin.site.register(HubStatus)
--- macAddr/mac/urls.py
+++
@@ -1,6 +0,0 @@
-"""import the view function"""
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("ping/",views.ping),
-]
